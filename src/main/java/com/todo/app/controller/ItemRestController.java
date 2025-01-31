962ed871b1e0c6b22603836884fda1a9e2ff3bce
tushark <tushark@steepgraph.com>
2025-01-31 14:31:26 +0530

    doc

--- src/main/java/com/todo/app/controller/ItemRestController.java
+++ src/main/java/com/todo/app/controller/ItemRestController.java
@@ -14,7 +14,11 @@ public class ItemRestController {
 
 	@Autowired
 	CommandBarMenuButtonService menuButtonService;
+	
 
+	/*
+	 * @description Need to add more mappings for each itemtype, ignores item_id for now
+	 */
 	@GetMapping("/api/{item_type_name}/{item_id}")
 	public List getItemForItemType(@PathVariable String item_type_name, @PathVariable String item_id) {
 		if (item_type_name.toLowerCase().equals("commandbarmenubutton")) {
