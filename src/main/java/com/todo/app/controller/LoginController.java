5bc6405527756571be99e13495162a76749d8371
tushark <tushark@steepgraph.com>
2025-02-05 14:50:32 +0530

    feat: Login added
    from @shivam-999-prog

--- src/main/java/com/todo/app/controller/LoginController.java
+++ src/main/java/com/todo/app/controller/LoginController.java
@@ -1,16 +1,19 @@
 package com.todo.app.controller;
 
+import java.security.*;
 import com.todo.app.entity.User;
 import com.todo.app.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.xml.bind.DatatypeConverter;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class LoginController {
@@ -19,16 +22,39 @@ public class LoginController {
     @Autowired
     UserService userService;
 
-    @PostMapping(path = "/auth/login", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Boolean login_user(@RequestBody User user) {
+    @PostMapping(
+            path = "/auth/login",
+            consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE}
+    )
+    public RedirectView login_user(@RequestParam Map<String, String> params) {
+        User user = new User();
+        user.loginName = params.get("login_name");
+        user.password = params.get("password");
         assert user != null;
         assert !user.loginName.isEmpty();
-        logger.info(user.loginName);
         List<User> users = userService.getMyUser(user.loginName);
         if (users.size() <= 0) {
             logger.warn("No user with given login_name found");
-            return false;
+            return new RedirectView("login", false);
         }
-        return users.get(0).password.equals(user.password);
+
+        byte[] byte_password = user.password.getBytes();
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            if(DatatypeConverter.printHexBinary( md.digest(byte_password)).toUpperCase().equals(users.get(0).password.toUpperCase())){
+                logger.info("User found redirecting");
+                return new RedirectView("/home",false);
+            }
+            
+        } catch (java.security.NoSuchAlgorithmException e){
+        }
+        finally{
+        }
+        logger.warn("login failed back to login page");
+        return new RedirectView("login", false);
+    }
+    @GetMapping(path = "/login")
+    public ModelAndView loginPage() {
+        return new ModelAndView("login");
     }
 }
